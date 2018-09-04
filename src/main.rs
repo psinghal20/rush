101e21659860a68b16555fd6159718955370933b
psinghal20 <psinghal20@gmail.com>
2018-09-05 02:55:31 +0530

    feat: implement exit,cd and command execution with background processes

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,49 @@
 extern crate libc;
-use std::process::Command;
+use std::env;
 use std::io;
+use std::path::Path;
+use std::process::Command;
 
 fn main() {
-    unsafe {
-        let child_pid = libc::fork();
-        
-        if child_pid == 0 {
-            println!("***Child***\nCurrent PID: {} and Child PID: {}\n", libc::getpid(), child_pid);
+    loop {
+        println!("RUSH!!!>>>>");
+        let mut command = String::new();
+        io::stdin()
+            .read_line(&mut command)
+            .expect("Failed to read the user command");
+        command.pop();
+        let mut command_tokens: Vec<&str> = command.split(' ').collect();
+        let mut is_background = false;
+        if let Some(&"&") = command_tokens.last() {
+            is_background = true;
+            command_tokens.pop();
+        }
+        match command_tokens[0] {
+            "exit" => std::process::exit(0),
+            "cd" => change_dir(command_tokens[1]),
+            _ => execute_command(command_tokens, is_background),
+        }
+    }
+}
+
+fn execute_command(command_tokens: Vec<&str>, is_background: bool) {
+    let mut command_instance = Command::new(command_tokens[0]);
+    if let Ok(mut child) = command_instance.args(&command_tokens[1..]).spawn() {
+        if is_background == false {
+            child.wait().expect("command wasn't running");
         }
         else {
-            let mut return_status = 0;
-            let return_pointer = &mut return_status as *mut i32;
-            libc::waitpid(child_pid, return_pointer ,libc::WUNTRACED);
-            println!("***Parent***\nCurrent PID: {} and Child PID: {}\n", libc::getpid(), child_pid);
+            println!("{} started!", child.id());
         }
+    } else {
+        println!("Command didn't start");
+    }
+}
 
-        let mut key_press = String::new();
-        io::stdin().read_line(&mut key_press).expect("Failed");
+fn change_dir(new_path: &str) {
+    let new_path = Path::new(new_path);
+    match env::set_current_dir(&new_path) {
+        Err(err) => println!("Failed to change the directory!\n{}", err),
+        _ => (),
     }
 }
