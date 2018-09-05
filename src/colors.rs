8652b0f2eb3c01a0bd64ee733d630beac60347ea
psinghal20 <psinghal20@gmail.com>
2018-09-06 00:50:21 +0530

    feat: implement ';' and refactor code for tokenizing

---
+++ src/colors.rs
@@ -0,0 +1,17 @@
+pub const RED: &str = "\x1b[31m";
+pub const GREEN: &str = "\x1b[32m";
+pub const ANSI_COLOR_YELLOW: &str = "\x1b[33m";
+pub const ANSI_COLOR_BLUE: &str = "\x1b[34m";
+pub const ANSI_COLOR_MAGENTA: &str = "\x1b[35m";
+pub const ANSI_COLOR_CYAN: &str = "\x1b[36m";
+pub const ANSI_BOLD: &str = "\x1b[1m";
+
+pub const RESET: &str = "\x1b[0m";
+
+pub fn error_logger(string: String) {
+    println!("{}{}{}", RED, string, RESET);
+}
+
+pub fn success_logger(string: String) {
+    println!("{}{}{}", GREEN, string, RESET);
+}
--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,46 @@ use std::env;
 use std::io;
 use std::path::Path;
 use std::process::Command;
+pub mod colors;
 
 fn main() {
     loop {
-        println!("RUSH!!!>>>>");
-        let mut command = String::new();
+        print_prompt();
+        let mut command_string = String::new();
         io::stdin()
-            .read_line(&mut command)
+            .read_line(&mut command_string)
             .expect("Failed to read the user command");
-        command.pop();
-        let mut command_tokens: Vec<&str> = command.split(' ').collect();
-        let mut is_background = false;
-        if let Some(&"&") = command_tokens.last() {
-            is_background = true;
-            command_tokens.pop();
-        }
-        match command_tokens[0] {
-            "exit" => std::process::exit(0),
-            "cd" => change_dir(command_tokens[1]),
-            _ => execute_command(command_tokens, is_background),
+
+        let commands_tokens = tokenize_commands(&mut command_string);
+
+        for mut command_tokens in commands_tokens {
+            let mut is_background = false;
+            if let Some(&"&") = command_tokens.last() {
+                is_background = true;
+                command_tokens.pop();
+            }
+            match command_tokens[0] {
+                "exit" => std::process::exit(0),
+                "cd" => change_dir(command_tokens[1]),
+                _ => execute_command(command_tokens, is_background),
+            }
         }
     }
+}
+
+fn print_prompt() {
+    let path = env::current_dir().unwrap();
+    println!("{}>> RUSHING IN {}{}{}",colors::ANSI_BOLD, colors::ANSI_COLOR_CYAN, path.display(), colors::RESET);
+}
+
+fn tokenize_commands(command_string: &mut String) -> Vec<Vec<&str> > {
+    command_string.pop();
+    let commands: Vec<&str> = command_string.split(';').collect();
+    let mut command_tokens: Vec<Vec<&str> > = Vec::new();
+    for command in commands.iter() {
+        command_tokens.push(command.split(' ').collect());
+    }
+    command_tokens
 }
 
 fn execute_command(command_tokens: Vec<&str>, is_background: bool) {
@@ -33,10 +52,10 @@ fn execute_command(command_tokens: Vec<&str>, is_background: bool) {
             child.wait().expect("command wasn't running");
         }
         else {
-            println!("{} started!", child.id());
+           colors::success_logger(format!("{} started!", child.id()));
         }
     } else {
-        println!("Command didn't start");
+        colors::error_logger("Command not found!".to_string());
     }
 }
 
